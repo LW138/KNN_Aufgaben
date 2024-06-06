29cd1e8d24242a6ce636345e28e34ac3be5847d6
Luca Wagner <luca.wagner1.lw@gmail.com>
2024-06-06 19:02:19 +0200

    Start Übung 6.2, Gefragt Trainings fehlen (OverFitting, Underfitting, Generalization)

--- Ubung6/uebung6_1.py
+++ Ubung6/uebung6_1.py
@@ -5,14 +5,9 @@ from Ubung5.uebung5_2 import MyDataset
 from Ubung5.uebung5_3 import Net
 from torch.utils.tensorboard import SummaryWriter
 
-def train_model(model, train_loader, val_loader, loss_function, optimizer, writer, num_epochs=20, device='cpu'):
-    # Move model to the specified device
+def train_model(model, train_loader, val_loader, loss_function, optimizer, writer, num_epochs=100, device='cpu'):
+    # Move model to the gpu if available, otherwise use the cpu by default
     model.to(device)
-
-    # Lists to store the losses to plot them afterwards
-    ##train_losses = []
-    #val_losses = []
-
     for epoch in range(num_epochs):
         # Set the model to training mode
         model.train()
@@ -24,26 +19,21 @@ def train_model(model, train_loader, val_loader, loss_function, optimizer, write
         for train_input, train_label in train_loader:
             # Move inputs and labels to the specified device
             train_input, train_label = train_input.to(device), train_label.to(device)
-
             # Zero the gradients
             optimizer.zero_grad()
-
             # Forward pass
             output = model(train_input)
-
             # Calculate the loss
             train_label = train_label.view(-1, 1).float()
             loss = loss_function(output, train_label)
             writer.add_scalar('Loss/train', loss, epoch)
             # Backward pass
             loss.backward()
-
             # Update the weights
             optimizer.step()
             running_loss += loss.item()
 
         train_loss = running_loss / len(train_loader)
-        #train_losses.append(train_loss)
 
         # Set the model to evaluation mode
         model.eval()
@@ -52,18 +42,16 @@ def train_model(model, train_loader, val_loader, loss_function, optimizer, write
             for val_input, val_label in val_loader:
                 # Move inputs and labels to the specified device
                 val_input, val_label = val_input.to(device), val_label.to(device)
-
+                writer.add_scalar('Loss/validate', loss, epoch)
                 outputs = model(val_input)
                 val_label = val_label.view(-1, 1).float()
                 loss = loss_function(outputs, val_label)
                 val_loss += loss.item()
 
         val_loss = val_loss / len(val_loader)
-        #val_losses.append(val_loss)
 
         print(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss}, Validation Loss: {val_loss}")
 
-    #return train_losses, val_losses
 
 def check_test_accuracy(model, test_loader, device='cpu'):
     model.eval()
@@ -97,14 +85,19 @@ valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
 
 model = Net(100, 10, 10, 1, weight_init='kaiming')
-loss_func = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.1)
+loss_func = nn.BCELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
 
+
+# start tensorboard with the following command: tensorboard --logdir=Ubung6/runs and open the web browser at http://localhost:6006/
 writer = SummaryWriter()
 
 train_model(model, train_loader, valid_loader, loss_func, optimizer, writer, device=device)
+
+
 writer.flush()
+writer.close()
 
 check_test_accuracy(model, test_loader, device)
 
-writer.flush()
+
---
+++ Ubung6/uebung6_2.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+import torchvision.datasets as datasets
+from torchvision import transforms
+from torch import optim
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader, random_split
+
+
+class FashionMNISTNet(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes, weight_init='xavier'):
+        super(FashionMNISTNet, self).__init__()
+        # Create three linear layers
+        self.layer1 = nn.Linear(input_size, hidden_size1)
+        self.layer2 = nn.Linear(hidden_size1, hidden_size2)
+        self.layer3 = nn.Linear(hidden_size2, num_classes)
+
+        #  based on the weight_init parameter, the weights of the linear layers are initialized
+        if weight_init == 'xavier':
+            init.xavier_uniform_(self.layer1.weight)
+            init.xavier_uniform_(self.layer2.weight)
+            init.xavier_uniform_(self.layer3.weight)
+        elif weight_init == 'kaiming':
+            init.kaiming_uniform_(self.layer1.weight, nonlinearity='relu')
+            init.kaiming_uniform_(self.layer2.weight, nonlinearity='relu')
+            init.kaiming_uniform_(self.layer3.weight, nonlinearity='relu')
+
+        init.zeros_(self.layer1.bias)
+        init.zeros_(self.layer2.bias)
+        init.zeros_(self.layer3.bias)
+
+    def forward(self, x):
+        # F is an alias for torch.nn.functional, which contains functions for many common operations on tensors
+        # Flatten the input tensor
+        x = x.view(x.size(0), -1)
+        out = F.relu(self.layer1(x))
+        out = F.relu(self.layer2(out))
+        out = F.softmax(self.layer3(out), dim=1)
+        return out
+
+    def train_model(self, train_loader, valid_loader, optimizer, loss_function, logger, num_epochs,  device='cpu'):
+        # Set the model to training mode
+        self.train()
+        self.to(device)
+        # Loop over the epochs
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            for i, data in enumerate(train_loader, 0):
+                # Get the inputs and labels
+                inputs, labels = data
+                # Move inputs and labels to the specified device
+                inputs, labels = inputs.to(device), labels.to(device)
+                # Zero the parameter gradients
+                optimizer.zero_grad()
+                # Forward pass
+                outputs = self(inputs)
+                # Calculate the loss
+                loss = loss_function(outputs, labels)
+                # Log the loss
+                logger.add_scalar('Loss/train', loss, epoch)
+                # Backward pass
+                loss.backward()
+                # Update the weights
+                optimizer.step()
+                # Add the loss to the running_loss
+                running_loss += loss.item()
+            train_loss = running_loss / len(train_loader)
+
+            # Set the model to evaluation mode
+            model.eval()
+            val_loss = 0.0
+            with torch.no_grad():
+                for val_input, val_label in valid_loader:
+                    val_input, val_label = val_input.to(device), val_label.to(device)
+                    logger.add_scalar('Loss/validate', loss, epoch)
+                    outputs = model(val_input)
+                    loss = loss_function(outputs, val_label)
+                    val_loss += loss.item()
+
+            val_loss = val_loss / len(valid_loader)
+
+            print(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss}, Validation Loss: {val_loss}")
+
+    def check_test_accuracy(self, test_loader, device='cpu'):
+        self.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for test_input, test_label in test_loader:
+                test_input, test_label = test_input.to(device), test_label.to(device)
+                outputs = self(test_input)
+                _, predicted = torch.max(outputs.data, 1)
+                total += test_label.size(0)
+                correct += (predicted == test_label).sum().item()
+
+        print(f"Accuracy on the test set: {100 * correct / total}%")
+
+if __name__ == "__main__":
+    # Define a transform to convert the images to tensors
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    dataset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
+
+    train_len = int(0.8 * len(dataset))
+    valid_len = int(0.1 * len(dataset))
+    test_len = len(dataset) - train_len - valid_len
+    train_dataset, valid_dataset, test_dataset = random_split(dataset, [train_len, valid_len, test_len])
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+    loss_func = nn.CrossEntropyLoss()
+
+    model = FashionMNISTNet(784, 16, 16, 10,  weight_init='xavier')
+
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    logger = SummaryWriter()
+
+    model.train_model(train_loader, valid_loader, optimizer, loss_func, logger, 20, device='cpu')
+    model.check_test_accuracy(test_loader, device='cpu')
